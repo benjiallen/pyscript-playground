34b6841183f053bdeacd293699e443768bacc49a
Ben Allen <ben.allen5@gmail.com>
2022-10-21 06:50:00 -0400

    testing search by name

---
+++ tests/test_search_by_name.py
@@ -0,0 +1,37 @@
+"""
+## Scenario: search by name with spaces
+
+Search "brian curtin"
+
+## Scenario: search by duplicate name
+
+Search "Guido van Rossum"
+expect "employees with same name" heading
+"""
+import pytest
+from playwright.sync_api import Page, expect, Locator
+
+@pytest.fixture(scope="function", autouse=True)
+def before_each_after_each(page: Page):
+    page.goto("http://0.0.0.0:8000/docs/org-chart.html")
+    page.get_by_label("Search by name").click()
+    yield
+
+def test_name_with_exact_match(page: Page) -> None:
+    """
+    Search "brian curtin"
+    Expect the result to be "Brian Curtin"
+    """
+    page.get_by_label("Search by name").fill("brian curtin")
+    page.get_by_role("button", name="Search").click()
+    expect(page.locator("#name")).to_have_text("Brian Curtin")
+
+def test_name_with_duplicate_matches(page: Page) -> None:
+    """
+    Search "Guido van Rossum"
+    Expect "employees with same name" heading
+    """
+    page.get_by_label("Search by name").fill("Guido van Rossum")
+    page.get_by_role("button", name="Search").click()
+    heading: Locator = page.get_by_role("heading", name="Employees with the same name")
+    expect(heading).to_be_visible()
